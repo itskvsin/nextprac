2c35ba1aa667d434b0d35f8aefcddb1213dde8bf
Kevin <kevinsolankistudy@gmail.com>
2025-06-23 22:53:01 +0530

    changed the bg of packages

--- src/components/Packages.tsx
+++ src/components/Packages.tsx
@@ -5,12 +5,12 @@ const packages = [
 ];
 
 export default function Packages() {
-  return (
-    <section className="py-12 bg-white text-black text-center">
+  return ( //
+    <section className="py-12 bg-[#F5A00D]  text-black text-center">
       <h2 className="text-4xl font-bold mb-8">Packages</h2>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 px-4 max-w-6xl mx-auto">
         {packages.map((pkg, i) => (
-          <div key={i} className="bg-gray-100 p-6 rounded-2xl shadow hover:scale-105 transition-transform">
+          <div key={i} className="bg-[#A37720] p-6 rounded-2xl border-[#F5A00D] border-1.5 shadow hover:scale-102 transition-transform">
             <h3 className="text-2xl font-semibold">{pkg.title}</h3>
             <p className="text-xl my-2">{pkg.price}</p>
             <ul className="space-y-1 text-left mt-4">
